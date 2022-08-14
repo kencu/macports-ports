1962cf8a6796d61274f87aee9052dbc2533159af
Ken Cunningham <kencu@macports.org>
2022-08-14 15:02:38 -0700

    blah2

---
+++ www/webkit2-gtk/files/include_wrappers/MacTypes.h
@@ -0,0 +1,8 @@
+//#define SavedRect Rect
+//#undef Rect
+//#define Rect MacTypesRect
+//#define Style MacTypesStyle
+#include_next <MacTypes.h>
+//#undef MacTypesRect
+//#undef Rect
+//#define Rect SavedRect
